cc842d7a8cc8e37ad2849847142fdd489033d918
Arthur Gustavo <artgustavobarros@gmail.com>
2025-05-11 07:52:36 -0300

    add testimonials animation

--- src/components/sections/testimonials/image-dialog.tsx
+++ src/components/sections/testimonials/image-dialog.tsx
@@ -22,7 +22,7 @@ export function ImageDialog({ index }: { index: number }) {
             height={400}
             className="rounded-lg object-cover group-hover:scale-125 duration-500 ease-in-out"
           />
-          <div className="bg-white rounded-full text-brand-500 absolute left-1/2 -translate-x-1/2 bottom-2 py-2 px-6 z-[1] font-martel font-bold group-hover:bg-brand-500 group-hover:text-white duration-500 ease-in-out">
+          <div className="bg-white rounded-full text-brand-500 absolute left-1/2 -translate-x-1/2 bottom-2 py-2 px-6 z-[1] font-martel font-bold group-hover:bg-brand-500 group-hover:text-white duration-500 ease-in-out transition-all">
             Visualizar
           </div>
         </div>
--- src/components/sections/testimonials/testimonials.tsx
+++ src/components/sections/testimonials/testimonials.tsx
@@ -8,28 +8,75 @@ import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Navigation, Pagination } from "swiper/modules";
 import { ImageDialog } from "./image-dialog";
+import { motion } from "motion/react";
+
+const container = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const items = {
+  left: {
+    hidden: {
+      opacity: 0,
+      x: -20,
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.6,
+      },
+    },
+  },
+  down: {
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+      },
+    },
+  },
+};
 
 export function Testimonials() {
   return (
-    <section className="pb-20">
+    <motion.section
+      className="pb-20"
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, amount: 0.4 }}
+      variants={container}
+    >
       <Container className="">
         <header className="flex items-end justify-between mb-10">
           <TitleWithTag
             orientation="vertical"
             textColor="text-gray-950"
             tagBgColor="bg-brand-500"
+            tagVariants={items.left}
+            textVariants={items.down}
           >
             Histórias reais. <br />
             Motivação para cumprir minha missão.
           </TitleWithTag>
-          <div className="hidden md:flex gap-2">
+          <motion.div className="hidden md:flex gap-2" variants={items.down}>
             <Button className="swiper-button-prev size-8 rounded-full bg-brand-100 cursor-pointer">
               <ChevronLeft className="text-brand-500" />
             </Button>
             <Button className="swiper-button-next size-8 rounded-full bg-brand-100 cursor-pointer">
               <ChevronRight className="text-brand-500" />
             </Button>
-          </div>
+          </motion.div>
         </header>
         <Swiper
           slidesPerView={1.3}
@@ -61,12 +108,17 @@ export function Testimonials() {
               key={index}
               className="max-h-[400px] rounded-lg overflow-hidden"
             >
-              <ImageDialog index={index} />
+              <motion.figure variants={items.down}>
+                <ImageDialog index={index} />
+              </motion.figure>
             </SwiperSlide>
           ))}
-          <div className="testimonials-pagination mt-4 h-14 md:hidden" />
+          <motion.div
+            className="testimonials-pagination mt-4 h-14 md:hidden"
+            variants={items.down}
+          />
         </Swiper>
       </Container>
-    </section>
+    </motion.section>
   );
 }
