fcea802a56eb8e5b23c6e038335cf3d26d461d05
Arthur Gustavo <artgustavobarros@gmail.com>
2025-05-12 12:26:31 -0300

    add work animation responsive

--- src/components/sections/works/work-swiper.tsx
+++ src/components/sections/works/work-swiper.tsx
@@ -5,14 +5,44 @@ import "swiper/css";
 import { WORKS } from "@/constants/works";
 import { CheckCircle2 } from "lucide-react";
 import { Pagination } from "swiper/modules";
+import { motion } from "motion/react";
+
+const sortedWorks = WORKS.sort((a, b) => a.mobileOrder - b.mobileOrder);
+const sanitizeWorks = [
+  [...sortedWorks.slice(0, 3)],
+  [...sortedWorks.slice(3, 6)],
+  [...sortedWorks.slice(6, 7)],
+];
+
+const container = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const items = {
+  down: {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+  },
+  top: {
+    hidden: { opacity: 0, y: -20 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+  },
+  left: {
+    hidden: { opacity: 0, x: -20 },
+    show: { opacity: 1, x: 0, transition: { duration: 0.6 } },
+  },
+  opacity: {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 1 } },
+  },
+};
 
 export function WorkSwiper() {
-  const sortedWorks = WORKS.sort((a, b) => a.mobileOrder - b.mobileOrder);
-  const sanitizeWorks = [
-    [...sortedWorks.slice(0, 3)],
-    [...sortedWorks.slice(3, 6)],
-    [...sortedWorks.slice(6, 7)],
-  ];
   return (
     <Swiper
       modules={[Pagination]}
@@ -21,14 +51,24 @@ export function WorkSwiper() {
     >
       {sanitizeWorks.map((cards, index) => (
         <SwiperSlide key={index}>
-          <div className="flex flex-col gap-8">
+          <motion.div
+            className="flex flex-col gap-8"
+            variants={container}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.4 }}
+          >
             {cards.map((item) => (
-              <div key={item.text} className="flex gap-5">
+              <motion.div
+                key={item.text}
+                className="flex gap-5"
+                variants={items.down}
+              >
                 <CheckCircle2 className="size-7 icon shrink-0 text-brand-500" />
                 <span>{item.text}</span>
-              </div>
+              </motion.div>
             ))}
-          </div>
+          </motion.div>
         </SwiperSlide>
       ))}
       <div className="works-pagination mt-4 h-14 sm:hidden" />
--- src/components/sections/works/work.tsx
+++ src/components/sections/works/work.tsx
@@ -4,12 +4,47 @@ import { TitleWithTag } from "../../title-with-tag";
 import { WorkList } from "./work-list";
 import { CheckCircle2 } from "lucide-react";
 import { WorkSwiper } from "./work-swiper";
+import * as motion from "motion/react-client";
 
+const sortedWorks = WORKS.sort((a, b) => a.tabletOrder - b.tabletOrder);
+
+const container = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const items = {
+  down: {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+  },
+  top: {
+    hidden: { opacity: 0, y: -20 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+  },
+  left: {
+    hidden: { opacity: 0, x: -20 },
+    show: { opacity: 1, x: 0, transition: { duration: 0.6 } },
+  },
+  opacity: {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 1 } },
+  },
+};
 export function Work() {
-  const sortedWorks = WORKS.sort((a, b) => a.tabletOrder - b.tabletOrder);
   return (
     <section>
-      <div className="hidden lg:block">
+      <motion.div
+        className="hidden lg:block"
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.4 }}
+      >
         <Container grid className="py-40">
           <header className="col-start-2 col-span-3">
             <TitleWithTag
@@ -17,6 +52,8 @@ export function Work() {
               textColor="text-gray-950"
               tagBgColor="bg-brand-500"
               className="sticky top-28"
+              tagVariants={items.left}
+              textVariants={items.down}
             >
               Como funciona o nosso trabalho
             </TitleWithTag>
@@ -25,42 +62,63 @@ export function Work() {
             <WorkList />
           </div>
         </Container>
-      </div>
-      <Container className="lg:hidden py-20 flex flex-col gap-10">
-        <header>
-          <div className="hidden md:block">
-            <TitleWithTag
-              orientation="vertical"
-              className="items-center"
-              textColor="text-gray-950"
-              tagBgColor="bg-brand-500"
-            >
-              Como funciona o nosso trabalho
-            </TitleWithTag>
+      </motion.div>
+      <motion.div
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
+        <Container className="lg:hidden py-20 flex flex-col gap-10">
+          <header>
+            <div className="hidden md:block">
+              <TitleWithTag
+                orientation="vertical"
+                className="items-center"
+                textColor="text-gray-950"
+                tagBgColor="bg-brand-500"
+                tagVariants={items.left}
+                textVariants={items.down}
+              >
+                Como funciona o nosso trabalho
+              </TitleWithTag>
+            </div>
+            <div className="md:hidden">
+              <TitleWithTag
+                orientation="vertical"
+                className=""
+                textColor="text-gray-950"
+                tagBgColor="bg-brand-500"
+                tagVariants={items.left}
+                textVariants={items.down}
+              >
+                Como funciona o nosso trabalho
+              </TitleWithTag>
+            </div>
+          </header>
+          <motion.ul
+            className="grid-cols-2 gap-16 hidden sm:grid"
+            variants={container}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+          >
+            {sortedWorks.map((item) => (
+              <motion.li
+                key={item.text}
+                className="flex gap-5"
+                variants={items.down}
+              >
+                <CheckCircle2 className="size-7 icon shrink-0 text-brand-500" />
+                <span>{item.text}</span>
+              </motion.li>
+            ))}
+          </motion.ul>
+          <div className="block sm:hidden">
+            <WorkSwiper />
           </div>
-          <div className="md:hidden">
-            <TitleWithTag
-              orientation="vertical"
-              className=""
-              textColor="text-gray-950"
-              tagBgColor="bg-brand-500"
-            >
-              Como funciona o nosso trabalho
-            </TitleWithTag>
-          </div>
-        </header>
-        <ul className="grid-cols-2 gap-16 hidden sm:grid">
-          {sortedWorks.map((item) => (
-            <li key={item.text} className="flex gap-5">
-              <CheckCircle2 className="size-7 icon shrink-0 text-brand-500" />
-              <span>{item.text}</span>
-            </li>
-          ))}
-        </ul>
-        <div className="block sm:hidden">
-          <WorkSwiper />
-        </div>
-      </Container>
+        </Container>
+      </motion.div>
     </section>
   );
 }
